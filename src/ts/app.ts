19decb39b0c30322c129559d1f328d63e26e7e01
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-01-18 03:29:10 +0300

    added storage and changed logic

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,6 +3,12 @@ const formTextfield: HTMLInputElement = document.querySelector('#form-textfield'
 const formAddButton: HTMLInputElement = document.querySelector('#form-add-button')
 const list: HTMLElement = document.querySelector('#list')
 
+let storage: any[] = [
+  { itemName: 'Learn javascript', id: Math.random(), completed: true },
+  { itemName: 'Create a todo list', id: Math.random(), completed: false },
+  { itemName: 'Create cooler projects', id: Math.random(), completed: false }
+]
+
 const handleSubmit = (e: any): void => {
   e.preventDefault()
 
@@ -10,21 +16,22 @@ const handleSubmit = (e: any): void => {
 
   formTextfield.value = ''
 
-  const item: string = createItem(itemName)
+  const item: object = { itemName, id: Math.random(), completed: false }
 
   addItem(item)
 
   bindEvents()
 }
 
-const addItem = (item: string): void => {
-  list.insertAdjacentHTML('beforeend', item)
+const addItem = (item: object): void => {
+  storage.push(item)
+  renderItems()
 }
 
-const createItem = (itemName: string): string => {
+const createItem = ({ itemName, id, completed }): string => {
   const item: string = `
-    <li class="list__item">
-      <button class="list__item-checkbox">&check;</button>
+    <li class="list__item" id="${id}">
+      <button class="list__item-checkbox ${completed ? 'list__item-checkbox--active' : ''}"}>&check;</button>
       <input type="text" class="list__item-textfield" value="${itemName}">
       <button class="list__item-delete-button">Delete</button>
     </li>
@@ -40,9 +47,16 @@ const handleItemClick = (e: any): void => {
   const item: HTMLElement = target.parentElement
 
   if (target.classList.contains('list__item-checkbox')) {
-    target.classList.toggle('list__item-checkbox--active')
+    storage = storage.map(storedItem => {
+      if (storedItem.id === +item.id) {
+        storedItem.completed = !storedItem.completed
+      }
+      return storedItem
+    })
+    renderItems()
   } else if (target.classList.contains('list__item-delete-button')) {
-    item.remove()
+    storage = storage.filter(storedItem => storedItem.id !== +item.id)
+    renderItems()
   }
 }
 
@@ -55,7 +69,19 @@ const bindEvents = (): void => {
   })
 }
 
+const renderItems = (): void => {
+  list.innerHTML = ''
+
+  storage.forEach(storedItem => {
+    const item: string = createItem(storedItem)
+
+    list.insertAdjacentHTML('beforeend', item)
+  })
+  
+  bindEvents()
+}
+
 inputForm.addEventListener('submit', handleSubmit)
 document.addEventListener("DOMContentLoaded", (e) => { 
-  bindEvents()
+  renderItems()
 })
